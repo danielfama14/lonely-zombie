98b9663a5fc87bd70b9245cb3a68fbc83aa9929c
danie <danielfama14@gmail.com>
2023-09-21 20:02:49 -0700

    Set up canvas and drafted routes

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,11 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+const gamepage = require('./gamepage');
 
 router.use('/', homeRoutes);
+router.use('/test', gamepage);
 router.use('/api', apiRoutes);
 
 module.exports = router;
+
